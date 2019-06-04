8d16a2425d5f38ff98296799aea02ff9aee5055e
Yassin AJDI <yassin.ajdi@gmail.com>
2019-06-04 20:14:14 +0000

    Setup basic navigation component

---
+++ app/src/main/java/com/ajdi/yassin/newsreader/ui/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.ajdi.yassin.newsreader.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ajdi.yassin.newsreader.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
